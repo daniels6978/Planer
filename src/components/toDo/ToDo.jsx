25d29f3a4d78e558363d2c5a9eb05d672bb228ef
RafalGrz <rafcio105@onet.pl>
2023-01-17 20:39:26 +0100

    taski

--- src/components/toDo/ToDo.jsx
+++ src/components/toDo/ToDo.jsx
@@ -1,27 +1,25 @@
-import React, { useState, useReducer } from "react";
-import { reducerT, initialStateT } from "../../helpers";
+import React, {useState, useReducer} from "react";
+import {reducerT, initialStateT} from "../../helpers";
 import AddTask from "./AddTask";
 import "./styleToDo.css";
 import Task from "./Task";
 
 function ToDo(props) {
-  if (!props.state?.user?.tasks) return null;
   const taskList = props.state.user.tasks;
 
   const [showAddTask, setShowAddTask] = useState(false);
 
   // const [state, dispatch] = useReducer(reducerT, initialStateT);
 
-  console.log(props.state);
-
   return (
     <>
       <div className="toDo_container">
         <h2 className="toDo_title">Zadania</h2>
         <div className="tasks">
-          {taskList?.map((task) => {
-            return <Task taskInfo={task} key={task.title} />;
-          })}
+          {props.state?.user?.tasks &&
+            taskList?.map((task) => {
+              return <Task taskInfo={task} key={task.title} />;
+            })}
           <button onClick={() => setShowAddTask(true)} className="addTaskBtn">
             <i className="fa fa-plus"></i>
           </button>
